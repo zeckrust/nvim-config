8df416bc695b7b15af2d3bd1ca818d2c8c1ce7cd
Gabriel Bergeron <gabriel.bergeron.protic@gmail.com>
2024-08-06 10:22:09 -0400

    added gdscript treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ return {
 	config = function()
 		local treesitter_config = require("nvim-treesitter.configs")
 		treesitter_config.setup({
-			ensure_installed = { "lua", "zig", "rust", "c" },
+			ensure_installed = { "lua", "zig", "rust", "c", "gdscript", "godot_resource", "gdshader" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
-	
+
 	end
 }
